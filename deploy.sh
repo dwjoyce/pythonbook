cb3710291a1ccf81e788156759554d94462cd457
Matthew Joyce <matsjoyce@gmail.com>
2015-07-08 19:33:50 +0100

    Fix missing sphinx on travis

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
+set -e
+
+echo "==> Setup"
+
 rm -rf pythonbook.wiki || exit 0;
+
+echo "==> Make"
 
 make latexpdf
 
-git clone https://github.com/dwjoyce/pythonbook.wiki.git
+echo "==> Clone"
 
 git config user.name "Travis CI"
 git config user.email ${GH_EMAIL}
+
+git clone https://github.com/dwjoyce/pythonbook.wiki.git
+
+echo "==> Move"
 
 cp build/latex/pythonbook.pdf pythonbook.wiki/
 
@@ -13,6 +23,13 @@ msg="Update to '`git log -1 --oneline`'"
 
 cd pythonbook.wiki
 
+echo "==> Commit"
+
 git add pythonbook.pdf
 git commit -m "$msg"
-git push https://matsjoyce:${GH_TOKEN}@github.com/dwjoyce/pythonbook.wiki.git
+
+echo "==> Push"
+
+git push https://matsjoyce:${GH_TOKEN}@github.com/dwjoyce/pythonbook.wiki.git master
+
+echo "==> Done!"
